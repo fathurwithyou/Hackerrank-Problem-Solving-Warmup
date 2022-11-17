471db1c03c993f4efa30915d9fda29d7602adeb4
fathurwithyou <82654955+fathurwithyou@users.noreply.github.com>
2022-11-17 19:34:10 +0700

    Create main.cpp

---
+++ 9828-simple-array-sum-English/9828-simple-array-sum-English.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef unsigned long long ull;
+using namespace std;
+
+void solve() {
+    int n, x;
+    cin >> n;
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        res += x;
+    }
+    cout << res;
+    return;
+}
+
+int main() {
+    solve();
+    return 0;
+}
